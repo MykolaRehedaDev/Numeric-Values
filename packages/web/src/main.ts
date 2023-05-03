8847042448009a8a6960310694c0495c05fb483a
MykolaRehedaDev <mykola.reheda.dev@gmail.com>
2023-05-03 16:01:38 +0300

    amplify configuration

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Amplify } from 'aws-amplify';
 
 import { AppModule } from './app/app.module';
+import awsmobile from './aws-exports';
 
-
+Amplify.configure(awsmobile);
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+.catch(err => console.error(err));
