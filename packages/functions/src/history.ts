4d7ea7482c592611a8b6ce16893d731756397f9b
MykolaRehedaDev <mykola.reheda.dev@gmail.com>
2023-05-03 14:10:46 +0300

    history function implemented

---
+++ packages/functions/src/history.ts
@@ -0,0 +1,28 @@
+import { ApiHandler } from "sst/node/api";
+import AWS from 'aws-sdk';
+
+import { constants } from './constants/constants';
+
+const queryClient = new AWS.TimestreamQuery({
+  region: 'eu-central-1',
+  accessKeyId: constants.ACCESS_KEY_ID,
+  secretAccessKey: constants.SECRET_ACCESS_KEY,
+})
+
+export const handler = ApiHandler(async (_evt) => {
+  const params = {
+    QueryString: 'SELECT * FROM "numericvalue"."values" ORDER BY "time" DESC',
+  };
+
+  let queryResponse;
+  await queryClient.query(params).promise()
+  .then((response) => {
+    queryResponse = JSON.stringify(response);
+    console.log('Success!');
+  })
+  .catch(() => {
+    console.error('Error while querying');
+  });
+
+  return JSON.parse(queryResponse).Rows;
+});
